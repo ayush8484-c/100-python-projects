a05e3121b53147e6fc3fce650e318e4bcf44f5a8
Ayush Srivastava <a28863442@gmail.com>
2026-04-10 08:29:00 +0530

    Add files via upload

---
+++ Day001_Band_Name_Generator/main.py
@@ -0,0 +1,4 @@
+print("Welome to the Band Name Generator")
+name1 = input("What's the name of the city you grew up in?\n")
+name2 = input("what's the name of your pet?\n")
+print("The name your band is" + " " + name1 + " " + name2)
