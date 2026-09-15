e61465ac54193c7a56fb62c1f21d5a819c172bd5
Ayush Srivastava <a28863442@gmail.com>
2026-09-15 22:11:01 +0530

    Add files via upload

---
+++ Day020_21_Snake_Game/main.py
@@ -0,0 +1,53 @@
+from turtle import Turtle, Screen
+from food import Food
+from snake import Snake
+from score import Scoreboard
+import time
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
